56c2eed2f8b3d0c3e2a7580a36450a59d6a1fb90
Galina <gal-dej@yandex.ru>
2023-01-20 15:42:27 +0300

    Task 2 has completed

---
+++ src/main/java/Task_2.java
@@ -0,0 +1,9 @@
+/* 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+ */
+import java.util.Scanner;
+public class Task_2 {
+    public static void main(String[] args) {
+        int n = (new Scanner(System.in).nextInt())%2;
+        System.out.println(n);
+    }
+}
